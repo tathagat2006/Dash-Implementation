dd35ffb023340ef053e23292fbe5cc118579c87a
Tathagat Thapliyal <tathagatthapliyal@Tathagats-MacBook-Pro.local>
2018-04-10 01:58:41 +0530

    TextBlob And Vader

---
+++ TextBlob.py
@@ -0,0 +1,86 @@
+# WE CAN USE TEXTBLOB OBJECT TO DO VARIOUS THINGS WITH IT.. LIKE TRANSLATE LANGUAGE , SENTIMENT ANALYSIS ETC..
+# from textblob import TextBlob
+
+# pos_count = 0
+# pos_correct = 0
+
+# with open("positive.txt","r") as f:
+#     for line in f.read().split('\n'):
+#         analysis = TextBlob(line)
+
+#         if analysis.sentiment.subjectivity < 0.3:
+#             if analysis.sentiment.polarity > 0:
+#                 pos_correct += 1
+#             pos_count +=1
+
+
+# neg_count = 0
+# neg_correct = 0
+
+# with open("negative.txt","r") as f:
+#     for line in f.read().split('\n'):
+#         analysis = TextBlob(line)
+#         if analysis.sentiment.subjectivity < 0.3:
+#             if analysis.sentiment.polarity <= 0:
+#                 neg_correct += 1
+#             neg_count +=1
+
+# print("Positive accuracy = {}% via {} samples".format(pos_correct/pos_count*100.0, pos_count))
+# print("Negative accuracy = {}% via {} samples".format(neg_correct/neg_count*100.0, neg_count))
+
+# from textblob import TextBlob
+
+# pos_count = 0
+# pos_correct = 0
+
+# with open("positive.txt","r") as f:
+#     for line in f.read().split('\n'):
+#         analysis = TextBlob(line)
+
+#         if analysis.sentiment.subjectivity > 0.9:
+#             if analysis.sentiment.polarity > 0:
+#                 pos_correct += 1
+#             pos_count +=1
+
+
+# neg_count = 0
+# neg_correct = 0
+
+# with open("negative.txt","r") as f:
+#     for line in f.read().split('\n'):
+#         analysis = TextBlob(line)
+#         if analysis.sentiment.subjectivity > 0.9:
+#             if analysis.sentiment.polarity <= 0:
+#                 neg_correct += 1
+#             neg_count +=1
+
+# print("Positive accuracy = {}% via {} samples".format(pos_correct/pos_count*100.0, pos_count))
+# print("Negative accuracy = {}% via {} samples".format(neg_correct/neg_count*100.0, neg_count))
+from textblob import TextBlob
+
+pos_count = 0
+pos_correct = 0
+
+with open("positive.txt","r") as f:
+    for line in f.read().split('\n'):
+        analysis = TextBlob(line)
+
+        if analysis.sentiment.polarity >= 0.5:
+            if analysis.sentiment.polarity > 0:
+                pos_correct += 1
+            pos_count +=1
+
+
+neg_count = 0
+neg_correct = 0
+
+with open("negative.txt","r") as f:
+    for line in f.read().split('\n'):
+        analysis = TextBlob(line)
+        if analysis.sentiment.polarity <= -0.5:
+            if analysis.sentiment.polarity <= 0:
+                neg_correct += 1
+            neg_count +=1
+
+print("Positive accuracy = {}% via {} samples".format(pos_correct/pos_count*100.0, pos_count))
+print("Negative accuracy = {}% via {} samples".format(neg_correct/neg_count*100.0, neg_count))
